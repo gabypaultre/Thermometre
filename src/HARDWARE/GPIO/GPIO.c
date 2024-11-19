28c4f94e5053a8fa0830cf0c1553e7e8268f7066
Gaby PAULTRE <gaby.paultre@esme.fr>
2024-11-19 10:04:41 +0100

    Développement de la partie logicielle pour piloter la LED

--- src/HARDWARE/GPIO/GPIO.c
+++ src/HARDWARE/GPIO/GPIO.c
@@ -4,14 +4,15 @@
  * Project: Projet Esme
  *
  ***********************************************************************************************************************
- * @file      main.c
+ * @file      GPIO.c
  *
- * @author    Jean DEBAINS
- * @date      14/06/2023
+ * @author    gpaultre
+ * @date      14/11/2024
  *
  * @version   0.0.0
  *
- * @brief     Main program entry point
+ * @brief     <you can add a description of your module here, otherwise please delete this line>
+ * @details   <you can add more details of your module here, otherwise please delete this line>
  *
  * @remark    Coding Language: C
  *
@@ -20,135 +21,56 @@
  ***********************************************************************************************************************
  */
 
-
-
 /**********************************************************************************************************************/
 /* INCLUDE FILES                                                                                                      */
 /**********************************************************************************************************************/
-#include <stdio.h>
-#include "Common.h"
 
-
-/*********************************/
-/* HARDWARE INCLUDES             */
-/*********************************/
-#include "ISR.h"
-#include "CLOCK.h"
-#include "ADC.h"
-#include "TIMER.h"
-#include "EUSART.h"
-#include "I2CM.h"
 #include "GPIO.h"
-
-// Add the required includes for the hardware modules here...
-
-/*********************************/
-/* DRIVER INCLUDES               */
-/*********************************/
-#include "LCD.h"
-
-// Add the required includes for the driver modules here...
-
-/*********************************/
-/* APPLICATION INCLUDES          */
-/*********************************/
-// Add the required includes for the application modules here...
-
 
 /**********************************************************************************************************************/
 /* CONSTANTS, MACROS                                                                                                  */
 /**********************************************************************************************************************/
 
-
-
 /**********************************************************************************************************************/
 /* TYPES                                                                                                              */
 /**********************************************************************************************************************/
-
-
 
 /**********************************************************************************************************************/
 /* PRIVATE VARIABLES                                                                                                  */
 /**********************************************************************************************************************/
 
-
-
 /**********************************************************************************************************************/
-/* PRIVATE FUNCTIONS PROTOTYPES                                                                                       */
+/* PRIVATE FUNCTION PROTOTYPES                                                                                        */
 /**********************************************************************************************************************/
-
-
 
 /**********************************************************************************************************************/
 /* PRIVATE FUNCTION DEFINITIONS                                                                                       */
 /**********************************************************************************************************************/
 
-
-
 /**********************************************************************************************************************/
-/* START FUNCTION                                                                                                     */
+/* PUBLIC FUNCTION DEFINITIONS                                                                                        */
 /**********************************************************************************************************************/
-void main(void)
+
+GPIO_status GPIO_init(void)
 {
-  // WDT operating mode ? WDT Disabled:
-  #pragma config WDTE = OFF
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 0;
 
-  // Low-voltage programming enabled:
-  #pragma config LVP = ON
-
-  // Enable the Global Interrupts
-  ISR_GlobalInterruptEnable();
-
-  // Enable the Peripheral Interrupts:
-  ISR_PeripheralInterruptEnable();
-
-  // Begin of the Init:
-  CMN_vidInitialize();
-
-  /*********************************/
-  /* HARDWARE INITIALIZATIONS      */
-  /*********************************/
-  CLOCK_vidInitialize();
-  EUSART_vidInitialize();
-  ADC_vidInitialize();
-  TIM0_vidInitialize();
-  I2CM_vidInitalize();
-  GPIO_init();
-
-  // Add your initialization function here for the hardware modules...
-
-  /*********************************/
-  /* DRIVER INITIALIZATIONS        */
-  /*********************************/
-  LCD_vidInitialize();
-
-  // Add your initialization function here for the driver modules...
-
-  /*********************************/
-  /* APPLICATION INITIALIZATIONS   */
-  /*********************************/
-  // Add your initialization function here for the application modules...
-
-  // End of the Init:
-  CMN_vidEndInit();
-
-  // Main program loop:
-  while(true)
-  {
-    GPIO_setGpioHigh();
-    __delay_ms(500);
-    GPIO_setGpioLow();
-
-    // Performs a delay of 0.5s:
-    __delay_ms(500);
-    
-    // Send a "Hello World" message to the serial port:
-    CMN_systemPrintf("Hello World ! \r\n"); // This message will be sent to the serial port on PUTTY
-  }
-
-  // We should never reach this code part:
-  CMN_abortAll();
+    return GPIO_OK;
 }
 
+GPIO_status GPIO_setGpioHigh(void)
+{
+    LATAbits.LATA4 = 1;
+
+    return GPIO_OK;
+}
+
+GPIO_status GPIO_setGpioLow(void)
+{
+    LATAbits.LATA4 = 0;
+
+    return GPIO_OK;
+}
 
 /*--------------------------------------------------------------------------------------------------------------------*/
